c2b054641e8f8f3857f5478045fd3284fd4d3b96
gliglob <hlliu@caltech.edu>
2015-09-29 20:49:22 -0400

    Update Model.py

--- Model.py
+++ Model.py
@@ -58,31 +58,40 @@ class TradeData():
     def __init__(self, Data):
         self.DF = Data
         
+#############
+#############
+# Pull Data #
+#############
+#############
 
-# Pull Data
 import tushare as ts
 import os 
 os.chdir('C:/Users/zklnu66/Desktop')
 
-# Industry
-StockInfo = ts.get_industry_classified()
-StockInfo = StockInfo.rename(columns = {'c_name': 'industry'})
+#################
+#  Fundamentals #
+#################
+
+# Industry, PE, Outstanding, PB, TimeToMarket, Concept
+
+StockInfo = ts.get_stock_basics()
 StockInfo = StockInfo.set_index('code')
-StockInfo = StockInfo.drop('name', 1)
+StockInfo = StockInfo.drop(['name', 'area', 'totals', 'totalAssets', 'liquidAssets', \
+    'fixedAssets', 'reserved', 'reservedPerShare', 'eps', 'bvps'], axis = 1)
 
 # Concept
 Concept = ts.get_concept_classified()
 Concept = Concept.set_index('code')
 Concept = Concept.rename(columns = {'c_name': 'concept'})
-Concept = Concept.drop('name', 1)
-# Load Concept Mapping from local drive
-concept_translation = 
+Concept = Concept.drop('name', axis = 1)
+# TODO Load Concept Mapping from local drive
+# concept_translation = 
 
 # Merge
 StockInfo = pd.merge(StockInfo, Concept, how = 'outer', on = 'code')
 
 # get all stocks
-StockName = Industry['code']
+StockName = StockInfo['code']
 StockName.to_csv('./ASHR/DATA/StockName.csv', index = False)
 
 # Small & Medium Enterprise
@@ -110,13 +119,64 @@ SZ50S.to_csv('./ASHR/DATA/SZ50S.csv', index = False)
 ZZ500S = ts.get_zz500s()['code']
 ZZ500S.to_csv('./ASHR/DATA/ZZ500S.csv', index = False)
 
-# Fund Holdings
+#################
+# Fund Holdings #
+#################
+
 # TODO Data is available quarterly
 FundHolding = ts.fund_holdings(CURRENT.year, np.floor((CURRENT.month+2)/3))
 
 
+####################
+# Financial Report #
+####################
+
+# FinancialReport: EPS, EPS_YOY, ROE, EPCF (cash flow per share),net_profits, profits_yoy
+# ProfitData: ROE, net_profit_ratio, gross_profit_rate, EPS, bips (business income per share)
+# GrowthData: mbrg (main business rate growth), nprg (net profit), 
+#             nav, targ (total asset), epsg, seg (shareholder's eqty)
+# DebtPayingData: currentratio, quickratio, cashratio, icratio (interest coverage)
+
+
+# TODO Data is available quarterly
+# TODO Compare data for FinancialReport and ProfitData
+
+FinancialData = ts.get_report_data(CURRENT.year, np.floor((CURRENT.month+2)/3))
+FinancialData = FinancialData.drop(['name', 'bvps', 'distrib', 'report_date'], axis = 1)
+
+ProfitData = ts.get_profit_data(CURRENT.year, np.floor((CURRENT.month+2)/3))
+ProfitData = ProfitData.drop(['name', 'bvps', 'distrib', 'report_date'], axis = 1)
+
+GrowthData = ts.get_growth_data(CURRENT.year, np.floor((CURRENT.month+2)/3))
+GrowthData = GrowthData.drop(['name'], axis = 1)
+
+DebtPayingData = ts.get_debtpaying_data(CURRENT.year, np.floor((CURRENT.month+2)/3))
+DebtPayingData = DebtPayingData.drop(['name', 'sheqratio', 'adratio'], axis = 1)
+
+# Merging data
+for subtab in [FinancialData, ProfitData, GrowthData, DebtPayingData]:
+    StockInfo = pd.merge(StockInfo, subtab, how = 'outer', on = 'code')
+
+# Saving data
+StockInfo = StockInfo.to_csv('./ASHR/DATA/StockInfo.csv', index = False)
+
+################
+##   Trade    ##
+################
+
+
+def WriteExcel(writer, sheet, df, index = False, header = True):
+    # Example: writer = pd.ExcelWriter('./ASHR/DATA/test.xlsx')
+    df.to_excel(writer, sheet, index = index, header = header)
+    writer.save()
+
+def ReadExcel(file, sheetname):
+    # Example: file = pd.ExcelFile('./ASHR/DATA/test.xlsx')
+    # read the sheet from the excel file
+    df = file.parse(sheetname)
+    return df
+    
 def main():
     # Example
     df = pd.DataFrame({'a':[1,2,4,5,9],'b':[2,3,1,6,15], 'c':[3,4,10,8,17]})
     df2 = pd.DataFrame({'a':[1,2,4,5,9],'bb':['a','b','c','d','e'], 'c':[3,4,10,8,18]})
-
