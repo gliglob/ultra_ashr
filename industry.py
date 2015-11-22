98cad3a5ee7b33e52b89a888d7fc9d4adfb7822b
gliglob <hlliu@caltech.edu>
2015-11-21 19:29:19 -0500

    Create industry.py

---
+++ industry.py
@@ -0,0 +1,15 @@
+['综合行业', '公路桥梁', '化纤行业', '机械行业', '生物制药', '石油行业', '玻璃行业', '仪器仪表', '交通运输', 
+ '飞机制造', '农林牧渔', '建筑建材',  '塑料制品', '商业百货', '纺织行业',  '医疗器械', '有色金属', '供水供气', 
+ '发电设备', '造纸行业', '船舶制造',  '煤炭行业', '食品行业', '陶瓷行业',  '纺织机械', '钢铁行业', '环保行业', 
+ '酿酒行业', '次新股', '电器行业',  '传媒娱乐', '化工行业', '房地产',  '金融行业', '其他行业', '开发区', 
+ '电子信息', '服装鞋类', '电子器件',  '电力行业', '汽车制造', '家具行业', '农药化肥', '酒店旅游',  '水泥行业',
+ '物资外贸', '摩托车', '印刷包装', '家电行业']
+ 
+ ['ConsumerDiscretionary', 'Industrials', 'Materials', 'Industrials', 'HealthCare', 'Energy', 'Materials', 
+  'Industrials', 'Industrials', 'Industrials', 'ConsumerStaples', 'Materials', 'Materials', 'ConsumerDiscretionary',
+  'ConsumerDiscretionary', 'HealthCare', 'Materials', 'Utilities', 'Industrials', 'Materials', 'Industrials',
+  'Materials', 'ConsumerStaples', 'Materials', 'Industrials', 'Materials', 'Utilities', 'ConsumerStaples', 'Financials', 
+  'ConsumerDiscretionary', 'ConsumerDiscretionary', 'Materials', 'Financials', 'Financials', 'Other', 'Other',
+  'InformationTechnology', 'ConsumerDiscretionary', 'ConsumerDiscretionary', 'Utilities', 'ConsumerDiscretionary', 
+  'ConsumerDiscretionary', 'Materials', 'ConsumerDiscretionary', 'Materials', 'Industrials', 'ConsumerDiscretionary',
+  'Materials', 'ConsumerDiscretionary']
