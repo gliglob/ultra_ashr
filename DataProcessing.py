45cb92bf2be9b25d535f8d9672af9c21347828f7
gliglob <hlliu@caltech.edu>
2015-11-24 23:15:54 -0500

    Create DataProcessing.py

---
+++ DataProcessing.py
@@ -0,0 +1,87 @@
+"""
+Data Processing
+"""
+
+from SetUp import *
+
+def TickDataProcessing(df, IntradayMasterClock):
+    """
+    Add the following features/columns:
+        /amount/:     price * volume
+        /orderType/:  'A' if amount >= 500000
+                      'B' if amount < 500000
+        /buyRatio/:   (Buy amount - Sell amount) / (Buy amount + Sell amount)
+        /A_buyRatio/: (A type Buy - A type Sell) / (A type Buy + A type Sell)
+                      NOTE: Inf values are changed to np.nan
+        /B_buyRatio/: (B type Buy - B type Sell) / (B type Buy + B type Sell)
+    """
+    
+    def OrderTypeMap(e):
+        return "A" if e >= 500000 else "B"
+        
+    df['amount'] = df['price'] * df['volume']
+    df['amountRolling'] = df['amount'].cumsum()
+    df['orderType'] = df['amount'].map(lambda x: OrderTypeMap(x))
+    #df['sidedAmount'] = df.apply(lambda x: x['amount'] if x['side'] == 'B' else -x['amount'], axis = 1)
+    df['volumeRolling'] = df['volume'].cumsum()
+    df['buyRollingSum'] = df['volume'][df['side'] == 'B'].cumsum()
+    df['sellRollingSum'] = df['volume'][df['side'] == 'S'].cumsum()
+    df = df.fillna(method = 'ffill').fillna(value = 0)
+    df['buyRatio'] = (df['buyRollingSum'] - df['sellRollingSum']) / (df['buyRollingSum'] + df['sellRollingSum'])
+    df = df.replace([-np.inf, np.inf], 0)
+    df['A_buyRollingSum'] = df['volume'][(df['side'] == 'B') & (df['orderType'] == 'A')].cumsum()
+    df['A_sellRollingSum'] = df['volume'][(df['side'] == 'S') & (df['orderType'] == 'A')].cumsum()
+    df['B_buyRollingSum'] = df['volume'][(df['side'] == 'B') & (df['orderType'] == 'B')].cumsum()
+    df['B_sellRollingSum'] = df['volume'][(df['side'] == 'S') & (df['orderType'] == 'B')].cumsum()
+    df = df.fillna(method = 'ffill').fillna(value = 0)
+    df['A_buyRatio'] = (df['A_buyRollingSum'] - df['A_sellRollingSum']) / (df['A_buyRollingSum'] + df['A_sellRollingSum'])
+    df['B_buyRatio'] = (df['B_buyRollingSum'] - df['B_sellRollingSum']) / (df['B_buyRollingSum'] + df['B_sellRollingSum'])
+    df = df.replace([-np.inf, np.inf], np.nan)
+    df = DropColumn(df, ['A_buyRollingSum', 'A_sellRollingSum', 'B_buyRollingSum', 'B_sellRollingSum', 'buyRollingSum', 'sellRollingSum', 'volumeRolling', 'side', 'volume', 'orderType'])
+    
+    # Take log of price, amount
+    df[['price', 'amount', 'amountRolling']] = np.log(df[['price', 'amount', 'amountRolling']])    
+   
+    ################################################################
+    # Note, Price, Amount, Volume Columns Are All On the Log Scale #
+    ################################################################
+#    df = df.rename(columns = {'price': 'logPrice', 'amount' : 'logAmount', 'amountRolling': 'LogAmountRolling', 'volumeRolling' : 'LogVolumeRolling'})
+    
+    # Asof join by MasterClock
+    df = df.set_index('time')
+    df = df.apply(lambda x: x.asof(IntradayMasterClock.MasterClock))
+    
+    return df
+    
+def SecondDataProcessing(df, IntradayMasterClock):
+    """
+    Generate 3 second master clock, as of join.
+    Add the following features:
+        /pendBuyRatio/:  Wei Bi (sum top five buy limit order volume - sum top five sell limit order volume) / (buy + sell)
+    """
+    df['pendingBuyVolume'] = df['buy1Volume'] + df['buy2Volume'] + df['buy3Volume'] + df['buy4Volume'] + df['buy5Volume']
+    df['pendingSellVolume'] = df['sell1Volume'] + df['sell2Volume'] + df['sell3Volume'] + df['sell4Volume'] + df['sell5Volume']
+    df['pendingBuyRatio'] = (df['pendingBuyVolume'] - df['pendingSellVolume']) / (df['pendingBuyVolume'] + df['pendingSellVolume']) 
+    df = DropColumn(df, ['exchange', 'ticker', 'pendingBuyVolume', 'pendingSellVolume', 'buy1Volume', 'buy2Volume', 'buy3Volume', 'buy4Volume', 'buy5Volume', 'sell1Volume', 'sell2Volume', 'sell3Volume', 'sell4Volume', 'sell5Volume', 'price'])    
+    ### TODO: Note, at the moment is pulling the date info from the dataframe
+    Date = df['time'].iloc[0].date()
+    MasterClock = AddDateToIntradayMasterClock(Date, IntradayMasterClock)
+    # Asof join by MasterClock
+    df = df.set_index('time')
+    df = df.apply(lambda x: x.asof(MasterClock.MasterClock))
+    return df
+
+def IndexDataProcessing(df, IntradayMasterClock):
+    """
+    Process the index data, generate 3 second master clock, as of join.
+    Output:
+        /df/: datetime index, price column
+    """
+    df = DropColumn(df, ['volume'])    
+    Date = df['time'].iloc[0].date()
+    MasterClock = AddDateToIntradayMasterClock(Date, IntradayMasterClock)
+    # Asof join by MasterClock
+    df = df.set_index('time')
+    df = df.apply(lambda x: x.asof(MasterClock.MasterClock))
+    df['price'] = np.log(df['price'])
+    return df
