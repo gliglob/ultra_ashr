a7ef48a2c8e586eb4ad8ea0a731e5652d7010081
gliglob <hlliu@caltech.edu>
2015-12-01 19:23:52 -0500

    Update SetUp.py

--- SetUp.py
+++ SetUp.py
@@ -57,34 +57,9 @@ logger.setLevel(logging.INFO)
 
 # Set Up Pandas Displays
 pd.set_option('display.width', 500)
-pd.set_option('display.max_columns', 50)
+pd.set_option('display.max_columns', 100)
 
 # AUTO-COMPLETION
 #import rlcompleter, readline
 #readline.parse_and_bind('tab:complete')
-
-##########
-# Static #
-##########
-
-TradingHours = [datetime.time(9, 30, 0), datetime.time(11, 30, 0), datetime.time(13, 0, 0), datetime.time(15, 0, 0)]
-
-# Load Holidays Schedule
-Holidays = pd.read_csv('./ASHR/DATA/Holidays.csv')
-Holidays['Holidays'] = TimeWrapper2(Holidays['Holidays'])
-HolidayList = Holidays['Holidays'].tolist()
-
-# Intraday Master Clock
-StartDate = datetime.datetime.combine(datetime.date(2015, 10, 9), TradingHours[0])
-EndDate = datetime.datetime.combine(datetime.date(2015, 10, 9), TradingHours[-1])
-Scale = '3s'
-IntradayMasterClock = GenerateMasterClock(StartDate, EndDate, Scale, TradingHours, HolidayList)
-IntradayMasterClock['MasterClock'] = IntradayMasterClock['MasterClock'].apply(lambda x: x.time())
-
-# Daily Stock Data for a single Stock
-DailyDataFrame = pd.DataFrame(columns = ['EndOfDayPendingBuyRatio', 'TotalAmount', 'Open', 'Close', 'High', 'Low', 
-    'BuyRatio', 'A_buyRatio', 'B_buyRatio', 'PriceSlope1', 'PriceCurvature1', 'PriceSlope2', 'PriceCurvature2', 
-    'PriceSlope3', 'PriceCurvature3', 'PendingBuySlope1', 'PendingBuyCurvature1', 'PendingBuySlope2', 'PendingBuyCurvature2', 
-    'PendingBuySlope3', 'PendingBuyCurvature3', 'AmountSlope1', 'AmountCurvature1', 'AmountSlope2', 'AmountCurvature2', 
-    'AmountSlope3', 'AmountCurvature3', 'IntegratedDiff1'])
 
