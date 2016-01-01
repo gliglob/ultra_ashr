ebed91e6d75a549595861d6b9b3c511849defed7
gliglob <hlliu@caltech.edu>
2015-12-31 21:36:26 -0500

    Update DailyDataFeature.py

--- DailyDataFeature.py
+++ DailyDataFeature.py
@@ -7,11 +7,19 @@ def DailyDataFeaturePrep(stock):
     """
     
     df = pd.read_csv(CONFIG.DAILYDATAPATH%stock)
-    Scale1 = 1
+    Scale1 = CONFIG.M1_1 / 4800
     Scale2 = CONFIG.M1_2 / 4800
     Scale3 = CONFIG.M1_3 / 4800
-    High_Scale2 = pd.rolling_max(df.Close, Scale2)
-    Close_Scale3 = pd.    
+    High_Scale2 = pd.rolling_max(df.High, Scale2, 0)
+    High_Scale2 = pd.rolling_max(df.High, Scale2, 0)
+    High_Scale3 = pd.rolling_max(df.High, Scale3, 0)
+    Low_Scale2 = pd.rolling_min(df.Low, Scale2, 0)
+    Low_Scale3 = pd.rolling_min(df.Low, Scale3, 0)
+    MA_Scale1 = df.PricePema1
+    MA_Scale2 = df.PricePema2
+    MA_Scale3 = df.PricePema3
+    Current = df.Close
+    
     
     # William %R
     # (High - Current) / (High - Low)
@@ -22,3 +30,5 @@ def DailyDataFeaturePrep(stock):
     df['WilliamR_2']
     
     # Inverse William %R
+
+df = pd.read_csv('/Users/Hui/Desktop/DailyData_SZ000001.csv', index_col = 'Time')
