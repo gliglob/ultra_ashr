87732124b9ba4ba472b5fea5a375c37b560af918
gliglob <hlliu@caltech.edu>
2016-01-03 21:46:11 -0500

    Update DailyDataFeature.py

--- DailyDataFeature.py
+++ DailyDataFeature.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from Config import CONFIG
 import pandas as pd
 
@@ -7,7 +8,7 @@ def DailyDataFeaturePrep(stock):
     """
     
     df = pd.read_csv(CONFIG.DAILYDATAPATH%stock)
-    Scale1 = CONFIG.M1_1 / 4800
+    Scale1 = int(np.ceil(CONFIG.M1_1 / 4800))
     Scale2 = CONFIG.M1_2 / 4800
     Scale3 = CONFIG.M1_3 / 4800
     High_Scale1 = pd.rolling_max(df.High, Scale1, 0)
@@ -24,7 +25,7 @@ def DailyDataFeaturePrep(stock):
     
     # William %R
     # (High - Current) / (High - Low)
-
+    
     df['WilliamR_1'] = (High_Scale1 - Current) / (High_Scale1 - Low_Scale1)
     df['WilliamR_2'] = (High_Scale2 - Current) / (High_Scale2 - Low_Scale2)
     df['WilliamR_3'] = (High_Scale3 - Current) / (High_Scale3 - Low_Scale3)
@@ -63,4 +64,4 @@ def DailyDataFeaturePrep(stock):
     df['EndOfDayPendingBuyRatio3'] = pd.rolling_mean(df.EndOfDayPendingBuyRatio, Scale3, 0)
     
 
-df = pd.read_csv('/Users/Hui/Desktop/DailyDataSZ000001.csv', index_col = 'Time')
+df = pd.read_csv('/Users/zklnu66/Desktop/DailyDataSZ000001.csv', index_col = 'Time')
