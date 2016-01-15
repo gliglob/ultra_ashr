ef5076d553e8b6c9a37c5f0bb1de8cb3f5e0c106
gliglob <hlliu@caltech.edu>
2016-01-14 20:47:22 -0500

    Update Modifications.py

--- Modifications.py
+++ Modifications.py
@@ -1,51 +1,24 @@
 """
-Modification 1:
-    Change the first row of the processed data, column sidedAmount and amount to match sidedAmountRolling and amountRolling
+Modification 3:
+    pull volume and volume rolling for sz000001
 """
-
-from Config import CONFIG
-from SetUp import *
-
-for stock in STOCK:
-    
-    
-    #########################
-    # Read and Process Data #
-    #########################
-    
-    logging.warning('Reading Data Start... For Stock %s From %s To %s...'%(stock, CONFIG.TRADINGDAYS[0], CONFIG.TRADINGDAYS[-1]))
-    
-    for date in CONFIG.TRADINGDAYS:
-        ProcessedDataPath = CONFIG.PROCESSEDDATAPATH%(stock, date)
-        
-        _checker = 0
-        while _checker <= 2:
-            _checker += 1
-            try: 
-                # Save Processed Daily Intraday 3sec Data
-                df = pd.read_csv(ProcessedDataPath, index_col = 'time')
-                df['sidedAmount'].iloc[0] = df['sidedAmountRolling'].iloc[0]
-                df['amount'].iloc[0] = df['amountRolling'].iloc[0]
-                df.to_csv(CONFIG.PROCESSEDDATAPATH%(stock, date))
-                break
-            except Exception, e:
-                if _checker == 1:
-                    logging.warning(str(e))
-                if _checker >= 3:
-                    if (not os.path.isfile(ProcessedDataPath)):
-                        logging.warning('Data for %s is missing on %s when reading data'%(stock, date))
-                    else:
-                        logging.warning('Other Critical Error When Reading Data of %s on %s'%(stock, date))
-
-"""
-Modification 2:
-    Add the volume column from raw tick data to prepare the VWAP
-"""
-
-for stock in STOCK:
-    #########################
-    # Read and Process Data #
-    #########################
-    
-    logging.warning('Reading Data Start... For Stock %s From %s To %s...'%(stock, CONFIG.TRADINGDAYS[0], CONFIG.TRADINGDAYS[-1]))
-    
+stock = 'SZ000001'
+for date in CONFIG.TRADINGDAYS:
+    ProcessedDataPath = CONFIG.PROCESSEDDATAPATH%(stock, date)
+    _checker = 0
+    while _checker <= 2:
+        _checker += 1
+        try: 
+            # Save Processed Daily Intraday 3sec Data
+            df = pd.read_csv(ProcessedDataPath, index_col = 'time')
+            df = df[['volume', 'volumeRolling']]
+            df.to_csv('/Users/Hui/Desktop/Volume_SZ000001/%s'%date)
+            break
+        except Exception, e:
+            if _checker == 1:
+                logging.warning(str(e))
+            if _checker >= 3:
+                if (not os.path.isfile(ProcessedDataPath)):
+                    logging.warning('Data for %s is missing on %s when reading data'%(stock, date))
+                else:
+                    logging.warning('Other Critical Error When Reading Data of %s on %s'%(stock, date))
