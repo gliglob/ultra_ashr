071a62a969a410bc6f43b6ab3bb577c9cc66c4e8
gliglob <hlliu@caltech.edu>
2016-01-15 17:29:25 -0500

    Update Config.py

--- Config.py
+++ Config.py
@@ -5,7 +5,7 @@ from Static import STATIC
 class CONFIG(object):
     
     # STOCK
-    STOCK = ['SZ000002']
+    STOCK = ['SZ000001']
     
     
     # File path
@@ -13,9 +13,16 @@ class CONFIG(object):
     SECONDDATAPATH = './ASHR/DATA/Test/Test_Second/%s/%s/%s_%s.csv'
     PROCESSEDFOLDERPATH = './ASHR/DATA/Processed/%s/'
     PROCESSEDDATAPATH = './ASHR/DATA/Processed/%s/%s.csv'
-    STOCKINDUSTRYMAPPATH = './ASHR/DATA/StockIndustryIndex.csv'
+    STOCKINDUSTRYMAPPATH = './ASHR/DATA/Index/IndustryIndex.csv'
     STOCKLISTPATH = './ASHR/DATA/Index/Index/csi_all.xls'
     INDEXDATAPATH = './ASHR/DATA/%s/%s.csv'
+    MODELPATH = './ASHR/DATA/Model/%s'
+    MODELDATAPATH = './ASHR/DATA/Model/%s/%s.pkl'
+    DAILYDATAPATH = './Dailydata_%s.csv'
+    DAILYVWAPDATAPATH = './Dailyvwapdata_%s.csv'
+    
+    # last model training update
+    LASTMODELUPDATE = '20160106'
     
     # Quantitative config
     TYPEA_CUTOFF = 500000
@@ -43,12 +50,23 @@ class CONFIG(object):
     # Daily Stock Data for a single Stock
     FEATURES = ['EndOfDayPendingBuyRatio', 'TotalAmount', 'Open', 'Close', 'High', 'Low', 
         'BuyRatio', 'A_buyRatio', 'B_buyRatio', 'PriceSlope1', 'PriceCurvature1', 'PriceSlope2', 'PriceCurvature2', 
-        'PriceSlope3', 'PriceCurvature3', 'PendingBuySlope1', 'PendingBuyCurvature1', 'PendingBuySlope2', 'PendingBuyCurvature2', 
+        'PriceSlope3', 'PriceCurvature3', 'PricePema1', 'PricePema2','PricePema3', 'PendingBuySlope1', 'PendingBuyCurvature1', 'PendingBuySlope2', 'PendingBuyCurvature2', 
         'PendingBuySlope3', 'PendingBuyCurvature3', 'AmountSlope1', 'AmountCurvature1', 'AmountSlope2', 'AmountCurvature2', 
         'AmountSlope3', 'AmountCurvature3', 'IndustrySpreadSlope1', 'IndustrySpreadCurvature1', 'IndustryeSpreadSlope2', 
-        'IndustrySpreadCurvature2', 'IndustrySpreadSlope3', 'IndustrySpreadCurvature3', 'SidedAmount1', 'SidedAmount2',
-        'SidedAmount3', 'Return1', 'Return2']
+        'IndustrySpreadCurvature2', 'IndustrySpreadSlope3', 'IndustrySpreadCurvature3', 'IndexSpreadSlope1', 'IndexSpreadCurvature1', 
+        'IndexSpreadSlope2', 'IndexSpreadCurvature2', 'IndexSpreadSlope3', 'IndexSpreadCurvature3', 'SidedAmount1', 'SidedAmount2',
+        'SidedAmount3', 'RSI1', 'RSI2', 'RSI3', 'Volatility', 'VolatilityIndexSpread', 'VolatilityIndustryIndexSpread', 'BetaIndex', 'BetaIndustryIndex', 'Return1', 'Return2', 'IncludedInTraining']
     DAILYDATAFRAME = pd.DataFrame(columns = FEATURES)
+    
+    VWAPFEATURES = ['Open', 'Close', 'High', 'Low', 'PriceSlope1', 'PriceCurvature1', 'PriceSlope2', 'PriceCurvature2', 
+    'PriceSlope3', 'PriceCurvature3', 'PricePema1', 'PricePema2','PricePema3','IndustrySpreadSlope1', 'IndustrySpreadCurvature1', 'IndustryeSpreadSlope2', 
+    'IndustrySpreadCurvature2', 'IndustrySpreadSlope3', 'IndustrySpreadCurvature3', 'IndexSpreadSlope1', 'IndexSpreadCurvature1', 
+    'IndexSpreadSlope2', 'IndexSpreadCurvature2', 'IndexSpreadSlope3', 'IndexSpreadCurvature3', 
+    'RSI1', 'RSI2', 'RSI3', 'Volatility', 'VolatilityIndexSpread', 'VolatilityIndustryIndexSpread', 'BetaIndex', 'BetaIndustryIndex']
+
+
+
+    DAILYVWAPDATAFRAME = pd.DataFrame(columns = VWAPFEATURES)
 
 
 
@@ -78,3 +96,18 @@ class CONFIG(object):
     STRATEGY1_SELLTHRESH = 0.01
     
     STRATEGY2_TRADINGHORIZON = 1
+    
+    
+    """
+    COMPLETE_FEATURES = ['EndOfDayPendingBuyRatio', 'TotalAmount', 'Open', 'Close', 'High',
+    'Low', 'BuyRatio', 'A_buyRatio', 'B_buyRatio', 'PriceSlope1', 'PriceCurvature1', 'PriceSlope2',
+    'PriceCurvature2', 'PriceSlope3', 'PriceCurvature3', 'PricePema1', 'PricePema2', 'PricePema3',
+    'PendingBuySlope1', 'PendingBuyCurvature1', 'PendingBuySlope2', 'PendingBuyCurvature2', 'PendingBuySlope3',
+    'PendingBuyCurvature3', 'AmountSlope1', 'AmountCurvature1', 'AmountSlope2', 'AmountCurvature2',
+    'AmountSlope3', 'AmountCurvature3', 'IndustrySpreadSlope1', 'IndustrySpreadCurvature1', 'IndustryeSpreadSlope2',
+    'IndustrySpreadCurvature2', 'IndustrySpreadSlope3', 'IndustrySpreadCurvature3', 'SidedAmount1', 'SidedAmount2',
+    'SidedAmount3', 'RSI1', 'RSI2', 'RSI3', 'Return1', 'Return2', 'WilliamR_1', 'WilliamR_2', 'WilliamR_3',
+    'InverseWilliamR_1', 'InverseWilliamR_2', 'InverseWilliamR_3', 'Disparity1', 'Disparity2', 'Disparity3',
+    'Disparity12', 'Disparity13', 'Disparity23', 'BuyRatio2', 'BuyRatio3', 'A_buyRatio2', 'A_buyRatio3',
+    'B_buyRatio2', 'B_buyRatio3', 'EndOfDayPendingBuyRatio2', 'EndOfDayPendingBuyRatio3']
+    """
